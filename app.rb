27304eb7bc1e41353104a726c99b0a427da5cb86
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-06 19:38:52 +0000

    added rock, paper, and scissor routes to rb file

--- app.rb
+++ app.rb
@@ -3,7 +3,27 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Welcome to Rock-Paper-Scissors!</h1>
   <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/rock") do
+  "
+  <h2>We played rock!</h2>
+  <h2>They played rock!</h2>
+  <h2>We tied!</h2>
+  "
+end
+
+get("/paper") do
+  "
+  <h2>We played paper!</h2>
+  "
+end
+
+get("/scissors") do
+  "
+  <h2>We played scissors!</h2>
   "
 end
